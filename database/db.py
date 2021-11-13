000998b7bc2b340a7823a4c1b8e6f357efe46e39
jack20081117 <530839656@qq.com>
2021-11-13 11:51:25 +0800

    增加数据库处理函数

---
+++ database/db.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+class Database(object):
+    def __init__(self,name):
+        self.filename=name
+        self.conn=sqlite3.connect(self.filename,check_same_thread=False)
+        self.cur=self.conn.cursor()
+
+    def dictFactory(self,cursor,row):
+        d={}
+        for index,col in enumerate(cursor.description):
+            d[col[0]]=row[index]
+        return d
+
+    def select(self,sql):
+        self.conn.row_factory=self.dictFactory
+        queryResult=self.cur.execute(sql).fetchall()
+        return queryResult
+
+    def execute(self,sql):
+        self.cur.execute(sql)
+        self.conn.commit()
+
+    def close(self):
+        self.cur.close()
+        self.conn.close()
