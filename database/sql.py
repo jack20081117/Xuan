abfba86e4489b6d7c7384c1aa7954eb1f3b6b65c
张珈瑞 <zjr2008jack@qq.com>
2021-12-10 20:22:44 +0800

    在数据库中增加createtime设置

--- database/sql.py
+++ database/sql.py
@@ -1,11 +1,11 @@
 selectTest='select * from t'
 
-selectGoData="select sgf,black,white from goData"
-selectGoDataPlus="select sgf,black,white from goData {}"
+selectGoData="select sgf,black,white,createtime from goData"
+selectGoDataPlus="select sgf,black,white,createtime from goData {}"
 selectFileRead="select count(*) as count from fileRead where filename='{}'"
 selectCountGoData='select count(1) as count from goData'
 
-insertGoData="insert into goData (sgf,black,white) values ('{}','{}','{}')"
+insertGoData="insert into goData (sgf,black,white,createtime) values ('{}','{}','{}','{}')"
 insertFileRead="insert into fileRead (filename) values ('{}')"
 
 vacuum='vacuum'
