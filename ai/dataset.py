25080748097bf3a123ca3aeaae65302c4773ce5c
张珈瑞 <zjr2008jack@qq.com>
2021-12-11 09:33:58 +0800

    完成数据基类

---
+++ ai/dataset.py
@@ -0,0 +1,20 @@
+from src.go import Go
+
+import torch
+from torch.utils.data import TensorDataset
+from torch.utils.data import Dataset
+
+DEVICE=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+GOBAN_SIZE=19
+
+class MyDataset(Dataset):
+    def __init__(self,datas):
+        super(MyDataset,self).__init__()
+        self.datas=datas
+
+    def __len__(self):
+        return len(self.datas)
+
+    def __getitem__(self,item):
+        data=self.datas[item]
+        return data
