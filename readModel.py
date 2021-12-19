a795e1b8ba1cd50dcad532f6f6fe3c0f4cba729c
张珈瑞 <zjr2008jack@qq.com>
2021-12-19 12:41:53 +0800

    完成读取模块

--- readModel.py
+++ readModel.py
@@ -3,10 +3,10 @@ from src.go import Go
 from ai.datacenter import DataCenter
 from config import GLOBAL_DICT as gl
 
-fileName='./config.ini'
+filePath='./config.ini'
 
 config=configparser.ConfigParser()
-config.read(fileName)
+config.read(filePath)
 port=config['tcp']['port']
 
 dbpath=os.path.dirname(os.path.realpath(__file__))
@@ -20,9 +20,50 @@ dataCenter=DataCenter()
 argv=sys.argv
 file=argv[1]
 
+def combine(data:str):
+    lines=data.split('\n')
+    result=''
+    for i in range(len(lines)):
+        result+=lines[i]
+    return result
+
 def saveFile(fileName,need):
     select=dataCenter.checkFileName(fileName)
     select=select[0]
+    if int(select['count']):
+        logging.info("该文件=[%s]已被解析过"%fileName)
+        dataCenter.vaccum()
+        if need is True:
+            return None
+        exit(0)
+    logging.info("正在解析%s"%fileName)
+    go=Go()
+    with open(fileName,'r',encoding='UTF-8') as f:
+        try:
+            line=f.read()
+        except UnicodeError:
+            logging.error("文件编码错误!")
+            return None
+        if need is True:
+            line=combine(line)
+        line=line.split('\n')
+        if len(argv)>3:
+            logging.info("大概有%d行数据"%len(line))
+            exit(0)
+        for i in range(len(line)):
+            logging.info("正在处理:%d"%i)
+            sgf=line[i]
+            if len(sgf)<10:continue
+            additional=go.parseAdditionalSgf(sgf)
+            logging.info('additional:{}'.format(additional))
+            try:
+                dataCenter.saveGoban(sgf,additional)
+            except sqlite3.IntegrityError:
+                logging.error("主键冲突 棋谱已经存在")
+            except sqlite3.OperationalError:
+                logging.error("出现错误")
+        logging.info("文件解析完毕 保存解析记录...")
+        dataCenter.saveCheckedName(fileName)
 
 if file=='del' or file=='delete':
     delete=input("即将删除所有围棋训练数据 输入[delete]以确认:\n")
