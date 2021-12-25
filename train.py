59213103b608c01c76887ddc3214dd149ced486a
张珈瑞 <zjr2008jack@qq.com>
2021-12-25 13:03:49 +0800

    完成测试训练模块;更新gitignore

--- train.py
+++ train.py
@@ -187,3 +187,95 @@ def test(dataSet):
         }))
         f.write('\n')
     logging.info('保存数据成功')
+
+def train(dataSet:MyDataset,time:int,testDataSet:list):
+    lr=float(config['ai'].get('LR',None))
+
+    inplane=int(config['ai'].get('inplane',None))
+    outplane=int(config['ai'].get('outplane',None))
+    outPlaneMap=int(config['ai'].get('outPlaneMap',None))
+    block=int(config['ai'].get('BLOCK',None))
+    ADAM=int(config['ai'].get('ADAM',None))
+    L2_REG=float(config['ai'].get('L2_REG',None))
+    MOMENTUM=float(config['ai'].get('MOMENTUM',None))
+    EPOCH=float(config['ai'].get('EPOCH',None))
+
+    if osType!='Windows' and EPOCH>4:
+        EPOCH=4
+
+    criterion=Loss()
+    feature,policy,value=loadModel(inplane,outplane,outPlaneMap,block)
+    dataLoader=DataLoader(dataSet,batch_size=batchSize,collate_fn=collateFn,shuffle=True,num_workers=5,drop_last=True)
+    jointParams=list(feature.parameters())+list(policy.parameters())+list(value.parameters())
+
+    optimizer=torch.optim.Adam(jointParams,lr=lr) if ADAM ==1 else torch.optim.SGD(jointParams,lr=lr,weight_decay=L2_REG,momentum=MOMENTUM)
+
+    logging.info('共%d个EPOCH'%EPOCH)
+    epochLoss=[]
+    for i in range(EPOCH):
+        batchLoss=[]
+        epochWinnerList=[]
+        for batchID,(state,winnerList,probasList) in enumerate(dataLoader):
+            length=len(state)
+            logging.info('这批数据共%d步棋'%length)
+            batchWinnerList=[]
+            singleLoss=[]
+            while length>=batchSize:
+                batchState=state[0:batchSize]
+                batchWinner=winnerList[0:batchSize]
+                batchProbas=probasList[0:batchSize]
+                batchState=torch.tensor(batchState,dtype=torch.float,device=DEVICE)
+                batchWinner=torch.tensor(batchWinner,dtype=torch.float,device=DEVICE)
+                batchProbas=torch.tensor(batchProbas,dtype=torch.float,device=DEVICE)
+                batchProbas=torch.reshape(batchProbas,(batchSize,-1))
+                optimizer.zero_grad()
+
+                featureMaps=feature(batchState.clone().detach())
+                winner=policy(featureMaps)
+                probas=value(featureMaps)
+                loss=criterion(winner,batchWinner,probas,batchProbas)
+                loss.backward()
+                optimizer.step()
+                singleLoss.append(float(loss))
+                state=state[batchSize:length]
+                winnerList=winnerList[batchSize:length]
+                probasList=probasList[batchSize:length]
+                length-=batchSize
+                wList=winner.tolist()
+                batchWinnerList.append(wList[0])
+            batchLoss.append(numpy.mean(singleLoss))
+            epochWinnerList.append(numpy.mean(batchWinnerList))
+            logging.info( "当前epoch=[%d] 共[%d]个,index=[%d] 到[%d]结束训练 批次loss=%d,time=%d"
+                          %(i+1,EPOCH,batchID+1,time,numpy.mean(singleLoss),getDatetime()['timeformat']))
+        logging.info("训练完一个epoch,开始测试")
+        try:
+            test(testDataSet)
+        except Exception as e:
+            logging.error("测试出现错误->%s"%e)
+        logging.info("Average backward pass->{}".format(numpy.mean(batchLoss)))
+        epochLoss.append(numpy.mean(batchLoss))
+        logging.info("Epoch loss:{}".format(epochLoss))
+        with open(log_fileName,encoding='utf-8',mode='a+') as f:
+            f.write(json.dumps({
+                'type':'loss',
+                'data':batchLoss
+            }))
+            f.write('\n')
+            f.write(json.dumps({
+                'type':'epochWinner',
+                'data':epochWinnerList
+            }))
+            f.write('\n')
+        saveModel(feature=feature,policy=policy,value=value)
+
+if __name__ == '__main__':
+    begintime=getDatetime()['timeformat']
+    data=getDataSet()
+    trainData=data[0:batchSize*100]
+
+    testData=data[batchSize*105:batchSize*105+5]
+    #test(testData)
+    myDataSet=MyDataset(trainData)
+    train(myDataSet,int(len(trainData)/batchSize),testData)
+    endtime=getDatetime()['timeformat']
+    logging.info("训练完毕,开始时间:%s,结束时间:%s"%(begintime,endtime))
