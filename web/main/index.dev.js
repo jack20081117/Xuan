ee28b015e34854f8d063ef33387708ada1cce6fe
张珈瑞 <zjr2008jack@qq.com>
2022-01-01 16:01:27 +0800

    complete web/main

---
+++ web/main/index.dev.js
@@ -0,0 +1,9 @@
+// Install `electron-debug` with `devtron`
+require('electron-debug')({showDevTools:true});
+
+require('electron').app.on('ready',()=>{
+    let installExtension=require('electron-devtools-installer');
+    installExtension.default(installExtension.VUEJS_DEVTOOLS).then(()=>{}).catch(err=>console.log('Unable to install `vue-devtools`: \n',err));
+});
+
+require('./index')
