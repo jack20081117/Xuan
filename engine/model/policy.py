bb0378bacfb23b8c1ef523eff23047b9c8ceae7f
张珈瑞 <zjr2008jack@qq.com>
2022-01-20 20:32:06 +0800

    no message

---
+++ engine/model/policy.py
@@ -0,0 +1,21 @@
+#策略器
+
+import torch.nn as nn
+import torch.nn.functional as functional
+from torch.nn.modules.module import Module
+
+class PolicyNet(Module):
+    def __init__(self,inplanes,outplanes):
+        super(PolicyNet,self).__init__()
+        self.outplanes=outplanes
+        self.conv=nn.Conv2d(inplanes,1,kernel_size=1)
+        self.bn=nn.BatchNorm2d(1)
+        self.logSoftMax=nn.LogSoftmax(dim=1)#加快运算效率,防止溢出
+        self.fc=nn.Linear(outplanes,outplanes)
+
+    def forward(self,x):#前向传播,求出梯度(这个函数应该是自己重写,torch自动调用)
+        x=functional.relu(self.bn(self.conv(x)))
+        x=x.view(-1,self.outplanes)
+        x=self.fc(x)
+        probas=self.logSoftMax(x).exp()
+        return probas
---
+++ engine/model/value.py
@@ -0,0 +1,32 @@
+#评价器
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as functional
+from torch.nn.modules.module import Module
+
+class ValueNet(Module):
+    '''
+    This network is used to predict which player is more likely to win given the input 'state'
+    described in the Feature Extractor model.
+    The output is a continuous variable, between -1 and 1.
+    '''
+    def __init__(self,inplanes,outplanes):
+        super(ValueNet,self).__init__()
+        self.outplanes=outplanes
+        self.conv=nn.Conv2d(inplanes,1,kernel_size=1)
+        self.bn=nn.BatchNorm2d(1)
+        self.fc1=nn.Linear(outplanes,256)
+        self.fc2=nn.Linear(256,1)
+
+    def forward(self,x):
+        '''
+        :param x:feature maps extracted from the state
+        :return:probability of the current agent winning the game,considering the actual state of the board
+        '''
+        x=functional.relu(self.bn(self.conv(x)))
+        #torch.Size([16,1,19,19])
+        x=x.view(-1,self.outplanes)
+        x=functional.relu(self.fc1(x))
+        winner=torch.tanh(self.fc2(x))
+        return winner
