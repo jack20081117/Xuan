7ce39653394a24d261eec3ba078a3eac3937bd20
张珈瑞 <zjr2008jack@qq.com>
2022-01-31 13:15:15 +0800

    update mcts

--- engine/ai/mcts.py
+++ engine/ai/mcts.py
@@ -1,7 +1,7 @@
 #蒙特卡洛树搜索模块
 #Monte Carlo Tree Search,简称MCTS
 
-import copy,logging
+import copy,logging,cmath,math
 from src.go import Go
 from ai.xuan import Xuan
 
@@ -13,7 +13,7 @@ class Node(object):
         self.probas=probas
         self.parent=parent#父节点
         self.children=[]#子树集合
-        self.num=0#遍历的次数
+        self.times=0#遍历的次数
         self.value=0#累计评分
 
     def __str__(self):
@@ -43,12 +43,12 @@ class MCTS(object):
     engine=None
 
     def __init__(self):
-        self.root=None
+        self.rootNode=None
         self.engine=Xuan()
 
     def analyze(self,goban):#蒙特卡洛树搜索 分析
         rootLegalMoves,rootProbas,rootWinner,rootString,rootBoard=self.engine.doAnalyze(goban=goban)
-        self.root=Node(string=rootString,board=rootBoard,legalMoves=rootLegalMoves,probas=rootProbas)
+        self.rootNode=Node(string=rootString,board=rootBoard,legalMoves=rootLegalMoves,probas=rootProbas)
         colorNum=1 if len(goban)==0 else Go.reverseColor(goban[-1]['color'])
         logging.info('这一步的合法落子点为 rootLegalMoves=%s'%rootLegalMoves)
         #把这步棋模拟的内容加上
@@ -65,14 +65,11 @@ class MCTS(object):
             success,board,string,robX,robY=self.engine.go.GoLogic(x=x,y=y,color=colorText)
             legalMoves,probas,winner,analyzeString,analyzeBoard=self.engine.doAnalyze(goban=tempGoban)
             logging.info('扩展的合法落子点 legalMoves:%s'%legalMoves)
-            self.root.expand(string=string,board=board,legalMoves=legalMoves,probas=probas)
+            self.rootNode.expand(string=string,board=board,legalMoves=legalMoves,probas=probas)
 
-    def mctExpand(self):
-        pass
-
-    def mctBackward(self,node:Node,num:int,value:int):#蒙特卡洛树搜索 回溯
-        node.num+=num
+    def backward(self,node:Node,times:int,value:int):#蒙特卡洛树搜索 回溯
+        node.times+=times
         node.value+=value
         if isinstance(node.parent,Node):
             parent=node.parent
-            self.mctBackward(node=parent,num=num,value=value)
+            self.backward(node=parent,times=times,value=value)
